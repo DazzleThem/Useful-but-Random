205f23c5bb29cb36b5e82b316b817618c5353cc4
Rebecca Leanne Coonen <38448044+DazzleThem@users.noreply.github.com>
2019-04-22 16:12:32 -0600

    Changed theta to go from 0 to 180 instead of 1 to 180. Updated comments to reflect

--- Mohr's Circle/Mohrs_Circle.m
+++ Mohr's Circle/Mohrs_Circle.m
@@ -1,18 +1,22 @@
-%This program graphs a Mohr's circle for given values of theta, normal
-%stress, and shear stress.
-%INPUTS 4
-%   t = theta, angle in degrees
+%This program graphs a Mohr's circle for input values of normal
+%stresses and shear stresses. Note the shear stresses will have the same
+%magnitude.
+%INPUTS 3
 %   sigmaxx = the value of normal stress in the x direction on the x face
 %   sigmayy = the value of normal stress in the y direction on the y face
 %   tauxy = the value of shear stress in the x or y direction on the x or y face
-t = 1; %set up t to be our counter in the for loop
+%DEFAULTS
+%Theta = t is always in degrees from 0 to 180
+
+
+t = 0; %set up t to be our counter in the for loop
 sigmaxx = input('Input value of normal stress on x face in x direction in ksi: ')
 fprintf('\n')
 sigmayy = input('Input value of normal stress on y face in y direction in ksi: ')
 tauxy = input('Input value of shear stresses in ksi: ')
 
 hold on %prevent values from being overwritten
-for t = 1:180
+for t = 0:180
     sigma = sigmaxx*cosd(t)*cosd(t)+ 2*tauxy*sind(t)*cosd(t) + sigmayy*sind(t)*sind(t); %this is the expression for total normal stress dervied in class
     tau = -sigmaxx*sind(t)*cosd(t) + tauxy*(cosd(t))^2 - tauxy*(sind(t))^2 + sigmayy*cosd(t)*sind(t); %the expression for total shear stress derived in class
     plot (sigma,tau ,'.')
